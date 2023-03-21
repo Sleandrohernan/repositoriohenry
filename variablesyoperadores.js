38f81da8884b46fb7a0422397b603c78b286ee28
leadnro sosa <leatemeprley22@gmail.com>
2023-03-21 15:31:42 -0300

    variablesyoperadores

---
+++ variablesyoperadores.js
@@ -0,0 +1,59 @@
+//variables
+
+var cajon1; //creando variable y asignandole un valor
+var cajon2;
+var cajon3;
+
+//tipos de datos
+
+//numericos y de tipo string (palabras)
+
+//string
+var nombre='henry';
+
+//numericos
+
+var numeroEntero= 17;
+
+var numeroNegativo= -4;
+
+var numeroDecimal= 11.34;
+
+//booleanos (verdadero o falso)
+
+var estoyEntendiendo= true;
+var estoyAburrido= false;
+
+//undefined
+
+var cajon1;
+
+console.log(cajon1);
+
+//null
+
+var sinNada= null;
+
+
+//operadores y precedencia
+
+//operadores
+
+//suma= +, resta= -, multiplicacion= *, division= /
+
+console.log(5+1);
+console.log(5-1);
+console.log(5*1);
+console.log(5/1);
+
+//presedencias = determina el orden en que los operadores son evaluados
+
+console.log(3+1*3-2);
+
+//se realizara la operacion con mayor precedencia, en este caso la multiplicacion.
+
+console.log((3+1)*(3-2));
+
+//en este caso se realizaran primero las operaciones dentro de los parentesis.
+
+
