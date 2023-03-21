9ce39b7af8a851ad886492c1b7a7b1d047032b5c
leadnro sosa <leatemeprley22@gmail.com>
2023-03-21 15:48:48 -0300

    operadoresdecomparacion

---
+++ operadoresdecomparacion.js
@@ -0,0 +1,24 @@
+
+// > mayor que
+// < menor que
+// >= mayor o igual que
+// <= menor o igual que
+// == igual que
+// != diferente que
+// = asignasion
+// === estricta
+
+// devuelven un valor booleano sobre si la operacion es verdadera o falsa
+
+console.log(4 > 7);
+console.log(4 < 7);
+console.log(4 >= 7);
+console.log(4 <= 7);
+console.log(4 == 7);
+
+//asociatividad = nos va a permitir identificar que operacion se ejecuta primero
+
+var a = 1;
+var b = 2;
+var c = a = b;
+
